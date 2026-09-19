5c05df67800eed2580c0c366d2323df02db87718
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-19 22:49:28 +0500

    updating

---
+++ 1.Numpy/2.Arrays.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+print(np.zeros((2,3)))
+
+print(np.ones((2,3)))
+
+print(np.eye(3))
+
+print(np.arange(0, 10, 2))
+
+print(np.linspace(0, 10, 5))
+
+print(np.random.randint(0, 100, (3, 4)))
+
+print(np.diag([1, 2, 3]))
+
+print(np.full((2, 3), 7))
