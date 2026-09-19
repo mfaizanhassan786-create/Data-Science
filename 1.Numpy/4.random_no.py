794212a5f44983bb366143de2eaa6ae06064767b
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-19 23:37:31 +0500

    updating

--- 1.Numpy/4.random_no.py
+++ 1.Numpy/4.random_no.py
@@ -1,14 +1,25 @@
 import numpy as np
 
-# Simulate 100k coin flips
-rng = np.random.default_rng()
-flips = rng.integers(0, 2, size=100000)  # 0 = tails, 1 = heads
-prob_heads = np.mean(flips)
-print("Estimated probability of heads:", prob_heads)
+# # Simulate 100k coin flips
+# rng = np.random.default_rng()
+# flips = rng.integers(0, 2, size=100000)  # 0 = tails, 1 = heads
+# prob_heads = np.mean(flips)
+# print("Estimated probability of heads:", prob_heads)
 
 
 # import time
 
-# # Python loop
+# # Python Loop
 # start = time.time()
 # squares = [i**2 for i in range(1000000)]
+# end = time.time()
+# print("Loop time:", end - start)
+
+# # NumPy vectorized
+# start = time.time()
+# arr = np.arange(1000000)
+# squares_np = arr**2
+# end = time.time()
+# print("Vectorized time:", end - start)
+
+
