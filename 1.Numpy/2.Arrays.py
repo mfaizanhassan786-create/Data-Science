5a2900f810fee99a43fd017dddde7f9bd93b6d78
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-19 22:53:10 +0500

    updating

--- 1.Numpy/2.Arrays.py
+++ 1.Numpy/2.Arrays.py
@@ -1,20 +1,29 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
-print(arr)
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr)
 
-print(np.zeros((2,3)))
+# print(np.zeros((2,3)))
 
-print(np.ones((2,3)))
+# print(np.ones((2,3)))
 
-print(np.eye(3))
+# print(np.eye(3))
 
-print(np.arange(0, 10, 2))
+# print(np.arange(0, 10, 2))
 
-print(np.linspace(0, 10, 5))
+# print(np.linspace(0, 10, 5))
 
-print(np.random.randint(0, 100, (3, 4)))
+# print(np.random.randint(0, 100, (3, 4)))
 
-print(np.diag([1, 2, 3]))
+# print(np.diag([1, 2, 3]))
 
-print(np.full((2, 3), 7))
+# print(np.full((2, 3), 7))
+
+
+
+# # changing the data type of the array
+
+
+# np.array([1, 2, 3, 4], dtype = "float")
+# arr = np.array([1.5, 2.7, 3.9])
+# print(arr.astype(int))
