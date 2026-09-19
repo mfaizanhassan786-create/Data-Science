fb5f5ab4b362a52e286f7aff4a02b42a105bdfcc
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-19 23:26:33 +0500

    updating

--- 1.Numpy/indexing-slicing-etc.py
+++ 1.Numpy/indexing-slicing-etc.py
@@ -1,9 +1,9 @@
-# import numpy as np
+import numpy as np
 
 
-# arr = np.array([[1, 2, 3],
-#                 [4, 5, 6],
-#                 [7, 8, 9]])
+arr = np.array([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
 
 # print(arr[0, 1])  #20 (rows0, col 1)
 # print(arr[2, 2]) #90 (row 2, col 2)
@@ -18,4 +18,15 @@
 # slice_arr = arr[0, :2]
 # slice_arr[0] = 999 
 
-# print(arr)
+# print(arr[0])
+
+
+copy_arr = arr[1].copy()  #independent copy
+copy_arr[0] = 555
+print(arr[1])    # original unchanged
+
+# Random generator with seed (reproducibility)
+
+rng = np.random.default_rng(seed=42)
+nums = rng.integers(1, 10, size=5)
+print(nums)
