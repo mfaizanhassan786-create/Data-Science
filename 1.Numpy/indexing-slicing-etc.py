764a7284c2bdeb82858f4b9c56fa9402ee5b3911
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-19 23:16:43 +0500

    updating

--- 1.Numpy/indexing-slicing-etc.py
+++ 1.Numpy/indexing-slicing-etc.py
@@ -1,21 +1,21 @@
-import numpy as np
+# import numpy as np
 
 
-arr = np.array([[1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]])
+# arr = np.array([[1, 2, 3],
+#                 [4, 5, 6],
+#                 [7, 8, 9]])
 
-print(arr[0, 1])  #20 (rows0, col 1)
-print(arr[2, 2]) #90 (row 2, col 2)
-print(arr[-1, -2]) #80 (last row, 2nd last col)
+# print(arr[0, 1])  #20 (rows0, col 1)
+# print(arr[2, 2]) #90 (row 2, col 2)
 
-print(arr[-3, -3])  # 10 (first row, first column)
+# print(arr[-1, -2]) #80 (last row, 2nd last col)
+# print(arr[-3, -3])  # 10 (first row, first column)
 
-# General form: arr[start : stop : step]
-print(arr[0:2, 0:2])  # rows 0-1, cols 1-2
-print(arr[::2, ::2])   # Every 2nd row and column 
+# # General form: arr[start : stop : step]
+# print(arr[0:2, 0:2])  # rows 0-1, cols 1-2
+# print(arr[::2, ::2])   # Every 2nd row and column 
 
-slice_arr = arr[0, :2]
-slice_arr[0] = 999 
+# slice_arr = arr[0, :2]
+# slice_arr[0] = 999 
 
-print(arr)
+# print(arr)
