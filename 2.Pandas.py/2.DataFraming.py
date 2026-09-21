8eadca90e12ec20c87bf80e9cb05834cf08cdc73
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-21 18:43:01 +0500

    updating

--- 2.Pandas.py/2.DataFraming.py
+++ 2.Pandas.py/2.DataFraming.py
@@ -1,23 +1,23 @@
-import os
-import pandas as pd
+# import os
+# import pandas as pd
 
-# data = {
-#     "Name": ["faizan" , "Faizan", "Ali", "Hassan"],
-#     "Age": [23, 24, 25, 27],
-#     "City": ["Karachi", "Lahore", "Islamabad", "Multan"]
-# }
+# # data = {
+# #     "Name": ["faizan" , "Faizan", "Ali", "Hassan"],
+# #     "Age": [23, 24, 25, 27],
+# #     "City": ["Karachi", "Lahore", "Islamabad", "Multan"]
+# # }
 
-# df = pd.DataFrame(data)
+# # df = pd.DataFrame(data)
 
-# print(df)
-
-
-# # Load sample CSV file
-
-df = pd.read_csv('2.Pandas.py/3.retail_sales.csv')
+# # print(df)
 
 
-# # Inspect Data Frame
+# # # Load sample CSV file
+
+# df = pd.read_csv('2.Pandas.py/3.retail_sales.csv')
+
+
+# # # Inspect Data Frame
 # print('Info : \n', df.info())
 
 # print('Shape : \n', df.shape)
