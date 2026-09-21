215f7849d0245f2fa1c9d243f695dea96dabf7a9
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-21 10:02:05 +0500

    Adding the new CSV file.

---
+++ 2.Pandas.py/2.DataFraming.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+data = {
+    "Name": ["faizan" , "Faizan", "Ali", "Hassan"],
+    "Age": [23, 24, 25, 27],
+    "City": ["Karachi", "Lahore", "Islamabad", "Multan"]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+
+# # Load sample CSV file
+
+# df = pd.read_csv('retail_sales.csv')
+
+# # Inspect Data Frame
+# print('Info : \n', df.info())
+
+# print('Shape : \n', df.shape)
+# print("Head : \n" , df.head(5))
+# print("Describe : \n", df.describe())
+# print("\nData Typess:\n" , df.dtypes)
+# print("\nStatistical Summary:\n" , df.describe())
+# print("\nNumber of rows and columns : \n" , (df.shape))
