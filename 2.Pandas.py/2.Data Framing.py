ad3b93fad74ca45b3bfd262670b34237ad5a109b
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-25 22:08:59 +0500

    updating

--- 2.Pandas.py/2.Data Framing.py
+++ 2.Pandas.py/2.Data Framing.py
@@ -52,22 +52,22 @@ import pandas as pd
 # print(df_emp[df_emp["Department"] == "IT"])
 
 
-# Example: Creating DataFrame with Custom Index & Accessing with loc / iloc:
+# # Example: Creating DataFrame with Custom Index & Accessing with loc / iloc:
 
-scores_data = {
-    "Math": [85, 92, 78, 90],
-    "Science": [88, 95, 72, 89],
-    "English": [79, 85, 88, 91]
-}
-students = ["Alice", "Bob", "Charlie", "David"]
+# scores_data = {
+#     "Math": [85, 92, 78, 90],
+#     "Science": [88, 95, 72, 89],
+#     "English": [79, 85, 88, 91]
+# }
+# students = ["Alice", "Bob", "Charlie", "David"]
 
-df_scores = pd.DataFrame(scores_data, index=students)
+# df_scores = pd.DataFrame(scores_data, index=students)
 
-print("\n--- Student Scores (Custom Index) ---")
-print(df_scores)
+# print("\n--- Student Scores (Custom Index) ---")
+# print(df_scores)
 
-print("\n--- Access by label using .loc['Bob'] ---")
-print(df_scores.loc["Bob"])
+# print("\n--- Access by label using .loc['Bob'] ---")
+# print(df_scores.loc["Bob"])
 
-print("\n--- Access by integer position using .iloc[0:2] ---")
-print(df_scores.iloc[0:2])
+# print("\n--- Access by integer position using .iloc[0:2] ---")
+# print(df_scores.iloc[0:2])
